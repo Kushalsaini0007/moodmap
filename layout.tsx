6abc99f2bcfdeb32160b22f38cd8a33e167b673d
Kushalsaini0007 <mrkushalsaini49@gmail.com>
2026-05-18 16:32:03 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MoodMap AI",
+  description: "Track your daily vibes with AI support",
+  verification: {
+    google: "8Xx4kG3G2F_iKazT2vp3FUpspo2-n1jd3YVbHn8vhiY",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
